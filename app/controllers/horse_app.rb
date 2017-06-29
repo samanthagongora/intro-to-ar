5615229df0139c786b391f7fb94f410045b9b68d
Samantha Gongora <samantha.gongora@gmail.com>
2017-06-28 21:59:27 -0600

    adds basic jockey and horse connection

--- app/controllers/horse_app.rb
+++ app/controllers/horse_app.rb
@@ -1,6 +1,14 @@
+# require './models/jockey.rb'
+# require '.models/horse.rb'
+
 class HorseApp < Sinatra::Base
   get '/horses' do
     @horses = Horse.all
     erb :"horses/index"
+  end
+
+  get '/jockeys' do
+    @jockeys = Jockey.all
+    erb :"jockeys/index"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818195543) do
+ActiveRecord::Schema.define(version: 20170629031735) do
 
   create_table "horses", force: :cascade do |t|
     t.text     "name"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160818195543) do
     t.integer  "total_winnings"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "jockey_id"
+  end
+
+  create_table "jockeys", force: :cascade do |t|
+    t.text     "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
